8d26a50459fb39a36b080775c23520d6795bb0a1
Shubham <shubhamkumar081908@gmail.com>
2024-11-07 17:39:56 +0530

    Abstraction

--- WildlifeConservationSimulation.cpp
+++ WildlifeConservationSimulation.cpp
@@ -9,9 +9,11 @@ private:
     int population;  
     string endangered_status;  
 
+    // Static members to keep track of total counts  
     static int total_species_count;  
     static int total_population;  
 
+    // Private method to automatically update status based on population  
     void update_status() {  
         if (population == 0) {  
             endangered_status = "Extinct";  
@@ -28,8 +30,7 @@ private:
 
 public:  
     // Constructor  
-    Species(const string& name, int population)  
-        : name(name), population(population) {  
+    Species(const string& name, int population) : name(name), population(population) {  
         total_species_count++;  
         total_population += population;  
         update_status();  
@@ -41,41 +42,32 @@ public:
         total_population -= population;  
     }  
 
-    // Static member functions  
+    // Getters for the private attributes  
+    string get_name() const { return name; }  
+    int get_population() const { return population; }  
+    string get_endangered_status() const { return endangered_status; }  
+
+    // Setter for population  
+    void set_population(int new_population) {  
+        if (new_population >= 0) {  
+            total_population -= population;  
+            population = new_population;  
+            total_population += population;  
+            update_status();  
+        }  
+    }  
+
+    // Function to display the species information  
+    void display() const {  
+        cout << name << ": Population " << population << ", Status: " << endangered_status << endl;  
+    }  
+
     static int get_total_species_count() {  
         return total_species_count;  
     }  
 
     static int get_total_population() {  
         return total_population;  
-    }  
-
-    // Accessor methods  
-    string get_name() const {  
-        return name;  
-    }  
-
-    int get_population() const {  
-        return population;  
-    }  
-
-    string get_endangered_status() const {  
-        return endangered_status;  
-    }  
-
-    // Mutator method to update population  
-    void set_population(int new_population) {  
-        if (new_population >= 0) {  
-            total_population -= population; // Subtract old population  
-            population = new_population; // Update to new population  
-            total_population += population; // Add updated population  
-            update_status(); // Update status accordingly  
-        }  
-    }  
-
-    // Function to display species information  
-    void display() const {  
-        cout << name << ": Population " << population << ", Status: " << endangered_status << endl;  
     }  
 };  
 
@@ -84,28 +76,38 @@ int Species::total_species_count = 0;
 int Species::total_population = 0;  
 
 int main() {  
-    // Creating species  
-    Species tiger("Tiger", 400);  
-    Species elephant("Elephant", 800);  
+    // Input variables for user  
+    string name;  
+    int population;  
+
+    // Getting user input for Species  
+    cout << "Enter the species name: ";  
+    cin >> name;  // Read species name  
+    cout << "Enter the initial population: ";  
+    cin >> population;  // Read species population  
+
+    // Creating species object based on user input  
+    Species user_species(name, population);  
 
     // Display initial species details  
-    tiger.display();  
-    elephant.display();  
+    user_species.display();  
 
-    // Accessing static member functions  
+    // Display total statistics  
     cout << "Total species count: " << Species::get_total_species_count() << endl;  
     cout << "Total population: " << Species::get_total_population() << endl;  
 
-    // Updating species population  
-    tiger.set_population(300);  
-    elephant.set_population(850);  
+    // Update population via user input  
+    int change;  
+    cout << "Enter the change in population (positive or negative): ";  
+    cin >> change;  
+
+    user_species.set_population(user_species.get_population() + change); // Update population  
 
     // Display updated species details  
     cout << "\nAfter population update:\n";  
-    tiger.display();  
-    elephant.display();  
+    user_species.display();  
 
-    // Accessing static member functions again  
+    // Display total statistics again  
     cout << "Total species count: " << Species::get_total_species_count() << endl;  
     cout << "Total population: " << Species::get_total_population() << endl;  
 
